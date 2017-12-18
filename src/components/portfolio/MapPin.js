b88d414c1ce3ab4ed2e9cb8971281ded6ef87926
Bedrock123 <info@builtbykingwilly.com>
2017-12-17 20:36:50 -0500

    Added in formal map marker

--- src/components/portfolio/MapPin.js
+++ src/components/portfolio/MapPin.js
@@ -1,21 +1,42 @@
 import React from 'react';
-import {Tooltip} from 'antd';
+import {Tooltip, Modal, Row, Col} from 'antd';
 import MapPinLocation from '../../assets/images/map-pin.png'
 
-class MapPin extends React.Component {
 
+class MapPin extends React.Component {
+constructor(props) {
+    super(props);
+        this.state = {
+            modalVisible: false
+        };
+    }
+    handleOk = (e) => {
+        this.setState({modalVisible: false});
+    }
+    handleCancel = (e) => {
+        this.setState({modalVisible: false});
+    }
+    showMapModal(record) {
+        if (this.state.visible) {
+            this.setState({modalVisible: false});
+        } else {
+            this.setState({modalVisible: true});
+        }
+    }
   render() {
 
     return (
+        <div>
         <Tooltip 
-            title="268 Dewey Rd"
+            title={this.props.address}
             placement="right"
         >
         <div
+            onClick={() => this.showMapModal()}
             style={{
                 position: 'absolute',
-                top: 1 + '%',
-                left: 9 + '%',
+                top: this.props.top + '%',
+                left: this.props.left + '%',
                 cursor: 'pointer'
             }}
         >
@@ -31,13 +52,103 @@ class MapPin extends React.Component {
                 color: 'white',
                 fontSize: 16 + 'px',
                 top: 8 + 'px',
-                left: 7 + 'px'
+                width: 83 + '%',
+                marginRight: 20 + 'px',
+                textAlign: 'center'
             
             }}>
-                268
+                {this.props.number}
             </p>
         </div>
      </Tooltip>
+     <Modal
+            closable={true}
+            style={{ 
+                top: 50,
+                width: 95 + '%',
+
+            }}
+            visible={this.state.modalVisible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+            footer={false}
+            maskClosable={true}
+        >
+            <br />
+            <img src={this.props.heroImageLocation} className={'modal-photo'} alt="Modal"/>
+            <br />
+            <h1 style={{
+                color: '#1d4a63',
+                textAlign: 'center',
+                fontSize: 26 + 'px'
+            }}>
+                {this.props.formalAddress}
+            </h1>
+            <h2 style={{
+                color: 'gray',
+                textAlign: 'center',
+                fontSize: 17 + 'px'
+            }}>
+                <i>{this.props.city}</i>
+            </h2>
+
+            <div
+                style={{
+                    padding: 122 + 'px',
+                    paddingBottom: 30 + 'px',
+                    paddingTop: 20 + 'px',
+                }}
+            >
+            <Row>
+                <Col xs={19}>
+                    <p style={{
+                        color: 'gray',
+                        textAlign: 'justify',
+                        fontSize: 16 + 'px',
+                        lineHeight: 1.2
+                    }}>
+                        {this.props.description}
+                    </p>
+                </Col>
+                <Col xs={1}/>
+                <Col xs={4}>
+                    <b className="modal-data-title">CLIENT</b>
+                    <p style={{
+                        color: 'gray',
+                        textAlign: 'left',
+                        fontSize: 14 + 'px',
+                        lineHeight: .8
+                    }}>
+                        <i>{this.props.client}</i>
+                    </p>
+                    <br />
+                    <b className="modal-data-title">ARCHITECT</b>
+                    <p style={{
+                        color: 'gray',
+                        textAlign: 'left',
+                        fontSize: 14 + 'px',
+                        lineHeight: .8
+                    }}>
+                        <i>{this.props.architect}</i>
+                    </p>
+                    <br />
+                    <b className="modal-data-title">LOCATION</b>
+                    <p style={{
+                        color: 'gray',
+                        textAlign: 'left',
+                        fontSize: 14 + 'px',
+                        lineHeight: .8
+                    }}>
+                        <i>{this.props.location}</i>
+                    </p>
+                    <br />
+                </Col>
+            </Row>
+            </div>
+            <br />
+            
+        </Modal>
+     </div>
 
     );
   }
--- src/containers/PeaPackGladstoneMap.js
+++ src/containers/PeaPackGladstoneMap.js
@@ -2,22 +2,39 @@ import React from 'react';
 import PeapackGladstoneMapLocation from '../assets/images/peapack-gladstone.jpg';
 import PortfolioFooter from '../components/portfolio/PortfolioFooter';
 import MapPin from '../components/portfolio/MapPin';
+import PeackPackGladstoneImageLocation from '../assets/images/modal_photo.png'
 
 class PeaPackGladstoneMap extends React.Component {
 
   render() {
 
     return (
-     <div>
+     <div
+        style={{
+          margin: 'auto',
+        }}
+     >
         <img 
             src={PeapackGladstoneMapLocation}
             alt="Peapack Gladstone Map" 
             style={{
-                width: 100 + '%'
+                width: 100 + '%',
+                margin: 'auto'
             }}
         />
-        <MapPin />
-       
+        <MapPin 
+          number={268} 
+          address={"268 Dewey Rd"}
+          top={1}
+          left={9}
+          formalAddress={'268 Dewey Rd.'}
+          city={'PeaPack Gladstone'}
+          description={'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui. Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam id dolor. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Donec sed odio dui.'}
+          heroImageLocation={PeackPackGladstoneImageLocation}
+          location={'Cum sociis natoque penatibus'}
+          architect={'Cum sociis'}
+          client={'Cum sociis'}
+        />
         <PortfolioFooter />
       </div>
     );
