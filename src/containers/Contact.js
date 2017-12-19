c651de2a41b03b1d5c86777333bbd6d00b1078e2
Kyle Lawson <Kyle.Lawson7@yahoo.com>
2017-12-19 12:50:05 -0500

    mobile responsive menu and contact page

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -4,11 +4,11 @@ import LocationBox from '../components/LocationBox';
 
 
 class Contact extends React.Component {
- 
+
   render() {
     return (
     <div>
-        <LocationBox 
+        <LocationBox
             mainHeader="INVESTOR"
             subHeader="Login"
             start="Home"
@@ -21,15 +21,7 @@ class Contact extends React.Component {
             backgroundOrigin: 'center',
             backgroundSize: 'cover'
         }}>
-            <div style={{
-                backgroundColor: '#a3a098',
-                opacity: 0.9,
-                height: 100 + '%',
-                width: 50 + '%',
-                float: 'right',
-                backgroundOrigin: 'center',
-                backgroundSize: 'contain'
-            }}>
+            <div className="map-overlay">
                 <h1 className="contact-main-header">
                     CONTACT
                 </h1>
@@ -44,21 +36,21 @@ class Contact extends React.Component {
                         190 MAIN STREET,  <br />
                         STE. 202, <br />
                         GLADSTONE, NJ 07934
-                    </h4>  
-                    <br />          
+                    </h4>
+                    <br />
                     <h3 className='contact-sub-header'>
                         CALL
                     </h3>
                     <h4 className="contact-sub-info">
                         1 + 908 234 9561
-                    </h4>  
-                    <br />          
+                    </h4>
+                    <br />
                     <h3 className='contact-sub-header'>
                         EMAIL
                     </h3>
                     <h4 className='contact-sub-info'>
                         info@MelilloEquities.com
-                    </h4>  
+                    </h4>
                 </div>
             </div>
         </div>
