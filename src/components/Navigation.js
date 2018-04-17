daefd61abd9e82321a6353691a6d320ba671fa80
Kyle Lawson <Kyle.Lawson7@yahoo.com>
2018-04-17 12:24:16 -0700

    menu updates

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -99,72 +99,145 @@ class Navigation extends React.Component {
 						/>
 					</a>
 
-					<div
-						className={menuClasses}
-						style={{
-							width: 240,
-							position: 'fixed',
-							top: 160
-						}}>
-						<Menu
-							theme="light"
-							openKeys={this.state.openKeys}
-							onOpenChange={this.onOpenChange}
-							selectedKeys={[window.location.pathname]}
-							defaultSelectedKeys={[this.state.urlLocation]}
-							mode="inline">
-							<Menu.Item key="/">
-								<NavLink to="/">
-									<span>HOME</span>
-								</NavLink>
-							</Menu.Item>
-
-							<Menu.Item key="/firm">
-								<NavLink to="/firm">
-									<span>FIRM</span>
-								</NavLink>
-							</Menu.Item>
-
-							<Menu.Item key="/leadership">
-								<NavLink to="/leadership">
-									<span>LEADERSHIP</span>
-								</NavLink>
-							</Menu.Item>
-							<Menu.Item key="/strategy">
-								<NavLink to="/strategy">
-									<span>STRATEGY</span>
-								</NavLink>
-							</Menu.Item>
-							<SubMenu key="sub1" title={'PORTFOLIO'}>
-								<Menu.Item key="/portfolio/bernardsville">
-									<NavLink to="/portfolio/bernardsville">Bernardsville</NavLink>
-								</Menu.Item>
-								<Menu.Item key="/portfolio/peapack-gladstone">
-									<NavLink to="/portfolio/peapack-gladstone">
-										Peapack-Gladstone
+					{this.props.windowWidth > 991 ? (
+						<div
+							className={menuClasses}
+							style={{
+								width: 240,
+								position: 'fixed',
+								top: 250
+							}}>
+							<Menu
+								theme="light"
+								openKeys={this.state.openKeys}
+								onOpenChange={this.onOpenChange}
+								selectedKeys={[window.location.pathname]}
+								defaultSelectedKeys={[this.state.urlLocation]}
+								mode="inline">
+								<Menu.Item key="/">
+									<NavLink to="/">
+										<span>HOME</span>
 									</NavLink>
 								</Menu.Item>
-								<Menu.Item key="/portfolio/farhills">
-									<NavLink to="/portfolio/farhills">Far Hills</NavLink>
+
+								<Menu.Item key="/firm">
+									<NavLink to="/firm">
+										<span>FIRM</span>
+									</NavLink>
 								</Menu.Item>
-								<Menu.Item key="/portfolio/mendham">
-									<NavLink to="/portfolio/mendham">Mendham</NavLink>
+
+								<Menu.Item key="/leadership">
+									<NavLink to="/leadership">
+										<span>LEADERSHIP</span>
+									</NavLink>
 								</Menu.Item>
-							</SubMenu>
-							<Menu.Item key="/investor-login">
-								<NavLink to="/investor-login">
-									<span>INVESTOR LOGIN</span>
-								</NavLink>
-							</Menu.Item>
-							<Menu.Item
-								key="/contact"
-								style={{ borderBottom: '1px solid #dddddd' }}>
-								<NavLink to="/contact">
-									<span>CONTACT</span>
-								</NavLink>
-							</Menu.Item>
-						</Menu>
-					</div>
+								<Menu.Item key="/strategy">
+									<NavLink to="/strategy">
+										<span>STRATEGY</span>
+									</NavLink>
+								</Menu.Item>
+								<SubMenu key="sub1" title={'PORTFOLIO'}>
+									<Menu.Item key="/portfolio/bernardsville">
+										<NavLink to="/portfolio/bernardsville">
+											Bernardsville
+										</NavLink>
+									</Menu.Item>
+									<Menu.Item key="/portfolio/peapack-gladstone">
+										<NavLink to="/portfolio/peapack-gladstone">
+											Peapack-Gladstone
+										</NavLink>
+									</Menu.Item>
+									<Menu.Item key="/portfolio/farhills">
+										<NavLink to="/portfolio/farhills">Far Hills</NavLink>
+									</Menu.Item>
+									<Menu.Item key="/portfolio/mendham">
+										<NavLink to="/portfolio/mendham">Mendham</NavLink>
+									</Menu.Item>
+								</SubMenu>
+								<Menu.Item key="/investor-login">
+									<NavLink to="/investor-login">
+										<span>INVESTOR LOGIN</span>
+									</NavLink>
+								</Menu.Item>
+								<Menu.Item
+									key="/contact"
+									style={{ borderBottom: '1px solid #dddddd' }}>
+									<NavLink to="/contact">
+										<span>CONTACT</span>
+									</NavLink>
+								</Menu.Item>
+							</Menu>
+						</div>
+					) : (
+						<div
+							className={menuClasses}
+							style={{
+								width: 240,
+								position: 'fixed',
+								top: 160
+							}}>
+							<Menu
+								theme="light"
+								openKeys={this.state.openKeys}
+								onOpenChange={this.onOpenChange}
+								selectedKeys={[window.location.pathname]}
+								defaultSelectedKeys={[this.state.urlLocation]}
+								mode="inline">
+								<Menu.Item key="/">
+									<NavLink to="/">
+										<span>HOME</span>
+									</NavLink>
+								</Menu.Item>
+
+								<Menu.Item key="/firm">
+									<NavLink to="/firm">
+										<span>FIRM</span>
+									</NavLink>
+								</Menu.Item>
+
+								<Menu.Item key="/leadership">
+									<NavLink to="/leadership">
+										<span>LEADERSHIP</span>
+									</NavLink>
+								</Menu.Item>
+								<Menu.Item key="/strategy">
+									<NavLink to="/strategy">
+										<span>STRATEGY</span>
+									</NavLink>
+								</Menu.Item>
+								<SubMenu key="sub1" title={'PORTFOLIO'}>
+									<Menu.Item key="/portfolio/bernardsville">
+										<NavLink to="/portfolio/bernardsville">
+											Bernardsville
+										</NavLink>
+									</Menu.Item>
+									<Menu.Item key="/portfolio/peapack-gladstone">
+										<NavLink to="/portfolio/peapack-gladstone">
+											Peapack-Gladstone
+										</NavLink>
+									</Menu.Item>
+									<Menu.Item key="/portfolio/farhills">
+										<NavLink to="/portfolio/farhills">Far Hills</NavLink>
+									</Menu.Item>
+									<Menu.Item key="/portfolio/mendham">
+										<NavLink to="/portfolio/mendham">Mendham</NavLink>
+									</Menu.Item>
+								</SubMenu>
+								<Menu.Item key="/investor-login">
+									<NavLink to="/investor-login">
+										<span>INVESTOR LOGIN</span>
+									</NavLink>
+								</Menu.Item>
+								<Menu.Item
+									key="/contact"
+									style={{ borderBottom: '1px solid #dddddd' }}>
+									<NavLink to="/contact">
+										<span>CONTACT</span>
+									</NavLink>
+								</Menu.Item>
+							</Menu>
+						</div>
+					)}
 				</BrowserView>
 
 				<MobileView device={isMobile}>
@@ -224,7 +297,7 @@ class Navigation extends React.Component {
 								margin: 'auto'
 							}}
 							strokeWidth={1}
-							color="white"
+							color="red"
 							animationDuration={0.5}
 						/>
 					</a>
