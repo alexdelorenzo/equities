9431347b2376e2c3c28aadcd49eed3a3b70e2e94
Kyle Lawson <Kyle.Lawson7@yahoo.com>
2018-04-17 10:22:03 -0700

    updated menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,12 @@ import { HamburgerButton } from 'react-hamburger-button'
 import { Menu } from 'antd'
 import logo from '../assets/images/logo.svg'
 import { NavLink } from 'react-router-dom'
+import {
+	BrowserView,
+	MobileView,
+	isBrowser,
+	isMobile
+} from 'react-device-detect'
 import windowSize from 'react-window-size'
 var classNames = require('classnames')
 const SubMenu = Menu.SubMenu
@@ -50,14 +56,15 @@ class Navigation extends React.Component {
 		})
 		return (
 			<div className="menu-container">
-				<NavLink to="/">
-					{this.props.windowWidth < 991 ? (
-					null
-					) : (
+				<BrowserView device={isBrowser}>
+					<NavLink to="/">
 						<div
 							className="icon-square"
 							style={{
-								position: 'fixed'
+								position: 'fixed',
+								height: 100,
+								width: 100,
+								padding: 14
 							}}>
 							<img
 								alt="Melillo Equities Logo"
@@ -65,17 +72,18 @@ class Navigation extends React.Component {
 								className="menu-logo"
 							/>
 						</div>
-					)}
-				</NavLink>
-
-				<a
-					className="hamburger-square"
-					style={{
-						position: 'fixed',
-						fontFamily: 'kepler-std-display'
-					}}
-					onClick={this.handleClick.bind(this)}>
-					{this.props.windowWidth > 991 ? (
+					</NavLink>
+					<a
+						className="hamburger-square"
+						style={{
+							position: 'fixed',
+							fontFamily: 'kepler-std-display',
+							height: 100,
+							width: 100,
+							padding: 35,
+							paddingTop: 37
+						}}
+						onClick={this.handleClick.bind(this)}>
 						<HamburgerButton
 							open={this.state.open}
 							onClick={this.handleClick.bind(this)}
@@ -89,7 +97,123 @@ class Navigation extends React.Component {
 							color="white"
 							animationDuration={0.5}
 						/>
+					</a>
+
+					<div
+						className={menuClasses}
+						style={{
+							width: 240,
+							position: 'fixed',
+							top: 160
+						}}>
+						<Menu
+							theme="light"
+							openKeys={this.state.openKeys}
+							onOpenChange={this.onOpenChange}
+							selectedKeys={[window.location.pathname]}
+							defaultSelectedKeys={[this.state.urlLocation]}
+							mode="inline">
+							<Menu.Item key="/">
+								<NavLink to="/">
+									<span>HOME</span>
+								</NavLink>
+							</Menu.Item>
+
+							<Menu.Item key="/firm">
+								<NavLink to="/firm">
+									<span>FIRM</span>
+								</NavLink>
+							</Menu.Item>
+
+							<Menu.Item key="/leadership">
+								<NavLink to="/leadership">
+									<span>LEADERSHIP</span>
+								</NavLink>
+							</Menu.Item>
+							<Menu.Item key="/strategy">
+								<NavLink to="/strategy">
+									<span>STRATEGY</span>
+								</NavLink>
+							</Menu.Item>
+							<SubMenu key="sub1" title={'PORTFOLIO'}>
+								<Menu.Item key="/portfolio/bernardsville">
+									<NavLink to="/portfolio/bernardsville">Bernardsville</NavLink>
+								</Menu.Item>
+								<Menu.Item key="/portfolio/peapack-gladstone">
+									<NavLink to="/portfolio/peapack-gladstone">
+										Peapack-Gladstone
+									</NavLink>
+								</Menu.Item>
+								<Menu.Item key="/portfolio/farhills">
+									<NavLink to="/portfolio/farhills">Far Hills</NavLink>
+								</Menu.Item>
+								<Menu.Item key="/portfolio/mendham">
+									<NavLink to="/portfolio/mendham">Mendham</NavLink>
+								</Menu.Item>
+							</SubMenu>
+							<Menu.Item key="/investor-login">
+								<NavLink to="/investor-login">
+									<span>INVESTOR LOGIN</span>
+								</NavLink>
+							</Menu.Item>
+							<Menu.Item
+								key="/contact"
+								style={{ borderBottom: '1px solid #dddddd' }}>
+								<NavLink to="/contact">
+									<span>CONTACT</span>
+								</NavLink>
+							</Menu.Item>
+						</Menu>
+					</div>
+				</BrowserView>
+
+				<MobileView device={isMobile}>
+					{this.state.open ? (
+						<div
+							className="icon-square"
+							style={{
+								position: 'fixed',
+								width: 0,
+								right: -10,
+								height: 120,
+								border: 'none',
+								padding: 'none'
+							}}>
+							<img
+								alt="Melillo Equities Logo"
+								src={logo}
+								className="menu-logo"
+								style={{ margin: 'auto', marginTop: 18 }}
+							/>
+						</div>
 					) : (
+						<div
+							className="icon-square"
+							style={{
+								position: 'fixed',
+								width: 0,
+								right: 0,
+								height: 120
+							}}>
+							<img
+								alt="Melillo Equities Logo"
+								src={logo}
+								className="menu-logo"
+								style={{ margin: 'auto', marginTop: 18 }}
+							/>
+						</div>
+					)}
+					<a
+						className="hamburger-square"
+						style={{
+							position: 'fixed',
+							fontFamily: 'kepler-std-display',
+							height: 60,
+							width: 60,
+							padding: 20,
+							paddingTop: 22.5
+						}}
+						onClick={this.handleClick.bind(this)}>
 						<HamburgerButton
 							open={this.state.open}
 							onClick={this.handleClick.bind(this)}
@@ -103,93 +227,86 @@ class Navigation extends React.Component {
 							color="white"
 							animationDuration={0.5}
 						/>
-					)}
-				</a>
+					</a>
 
-				<div
-					className={menuClasses}
-					style={{
-						width: 240,
-						position: 'fixed'
-					}}>
-					<Menu
-						theme="light"
-						openKeys={this.state.openKeys}
-						onOpenChange={this.onOpenChange}
-						selectedKeys={[window.location.pathname]}
-						defaultSelectedKeys={[this.state.urlLocation]}
-						mode="inline">
-
-
-						{this.props.windowWidth < 991 ? (
-							<Menu.Item key="icon" style={{height: '118px'}}>
-							<div
-							className="icon-square"
-							>
-							<img
-								alt="Melillo Equities Logo"
-								src={logo}
-								className="menu-logo"
-							/>
-						</div>
-							</Menu.Item>
-							) : (
-							null
-							)}
-
-			
-						<Menu.Item key="/">
-							<NavLink to="/">
-								<span>HOME</span>
-							</NavLink>
-						</Menu.Item>
-
-						<Menu.Item key="/firm">
-							<NavLink to="/firm">
-								<span>FIRM</span>
-							</NavLink>
-						</Menu.Item>
-
-						<Menu.Item key="/leadership">
-							<NavLink to="/leadership">
-								<span>LEADERSHIP</span>
-							</NavLink>
-						</Menu.Item>
-						<Menu.Item key="/strategy">
-							<NavLink to="/strategy">
-								<span>STRATEGY</span>
-							</NavLink>
-						</Menu.Item>
-						<SubMenu key="sub1" title={'PORTFOLIO'}>
-							<Menu.Item key="/portfolio/bernardsville">
-								<NavLink to="/portfolio/bernardsville">Bernardsville</NavLink>
-							</Menu.Item>
-							<Menu.Item key="/portfolio/peapack-gladstone">
-								<NavLink to="/portfolio/peapack-gladstone">
-									Peapack-Gladstone
+					<div
+						className={menuClasses}
+						style={{
+							width: 240,
+							position: 'fixed'
+						}}>
+						<Menu
+							theme="light"
+							openKeys={this.state.openKeys}
+							onOpenChange={this.onOpenChange}
+							selectedKeys={[window.location.pathname]}
+							defaultSelectedKeys={[this.state.urlLocation]}
+							mode="inline">
+							<Menu.Item key="icon" style={{ height: '118px' }}>
+								<NavLink to="/">
+									<div className="icon-square">
+										<img
+											alt="Melillo Equities Logo"
+											src={logo}
+											className="menu-logo"
+										/>
+									</div>
 								</NavLink>
 							</Menu.Item>
-							<Menu.Item key="/portfolio/farhills">
-								<NavLink to="/portfolio/farhills">Far Hills</NavLink>
+
+							<Menu.Item key="/">
+								<NavLink to="/">
+									<span>HOME</span>
+								</NavLink>
 							</Menu.Item>
-							<Menu.Item key="/portfolio/mendham">
-								<NavLink to="/portfolio/mendham">Mendham</NavLink>
+
+							<Menu.Item key="/firm">
+								<NavLink to="/firm">
+									<span>FIRM</span>
+								</NavLink>
 							</Menu.Item>
-						</SubMenu>
-						<Menu.Item key="/investor-login">
-							<NavLink to="/investor-login">
-								<span>INVESTOR LOGIN</span>
-							</NavLink>
-						</Menu.Item>
-						<Menu.Item
-							key="/contact"
-							style={{ borderBottom: '1px solid #dddddd' }}>
-							<NavLink to="/contact">
-								<span>CONTACT</span>
-							</NavLink>
-						</Menu.Item>
-					</Menu>
-				</div>
+
+							<Menu.Item key="/leadership">
+								<NavLink to="/leadership">
+									<span>LEADERSHIP</span>
+								</NavLink>
+							</Menu.Item>
+							<Menu.Item key="/strategy">
+								<NavLink to="/strategy">
+									<span>STRATEGY</span>
+								</NavLink>
+							</Menu.Item>
+							<SubMenu key="sub1" title={'PORTFOLIO'}>
+								<Menu.Item key="/portfolio/bernardsville">
+									<NavLink to="/portfolio/bernardsville">Bernardsville</NavLink>
+								</Menu.Item>
+								<Menu.Item key="/portfolio/peapack-gladstone">
+									<NavLink to="/portfolio/peapack-gladstone">
+										Peapack-Gladstone
+									</NavLink>
+								</Menu.Item>
+								<Menu.Item key="/portfolio/farhills">
+									<NavLink to="/portfolio/farhills">Far Hills</NavLink>
+								</Menu.Item>
+								<Menu.Item key="/portfolio/mendham">
+									<NavLink to="/portfolio/mendham">Mendham</NavLink>
+								</Menu.Item>
+							</SubMenu>
+							<Menu.Item key="/investor-login">
+								<NavLink to="/investor-login">
+									<span>INVESTOR LOGIN</span>
+								</NavLink>
+							</Menu.Item>
+							<Menu.Item
+								key="/contact"
+								style={{ borderBottom: '1px solid #dddddd' }}>
+								<NavLink to="/contact">
+									<span>CONTACT</span>
+								</NavLink>
+							</Menu.Item>
+						</Menu>
+					</div>
+				</MobileView>
 			</div>
 		)
 	}
